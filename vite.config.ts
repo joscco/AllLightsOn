0ce2cac39c482903734dcfd9b39bfbfa2cc3b5fc
Jonathan Schmitz <jomitz96@hotmail.com>
2024-10-30 12:05:08 +0100

    Add vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
-    base: '/AllLightsOn',
+    base: './',
     build: {
         rollupOptions: {
             plugins: [
